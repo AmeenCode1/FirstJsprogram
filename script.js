5b3cbe09096a04fae0aead4ce14cad112ee1283e
Ameen Memon <144584496+AmeenCode1@users.noreply.github.com>
2024-11-19 03:05:02 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let username = prompt("Enter your Name");
+let userage = prompt("Enter your age");
+
+alert("Hello, " + username + "!");
+
+if (userage >= 18) {
+    alert("You're an Adult Motherfucker");
+} else {
+    alert("Hi Pookie");
+}
